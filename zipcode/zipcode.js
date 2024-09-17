5ac89fca8c1d7cc535484df182c7efceede4bffd
odeilly <odeilly@gmail.com>
2024-09-18 08:42:09 +0900

    関数化

--- zipcode/zipcode.js
+++ zipcode/zipcode.js
@@ -19,7 +19,7 @@
 			})
 			.fail(() => {
 				console.log("error");
-				resultsArea.append('<span class="error">エラーが発生しました。</span>');
+				resultsArea.append(makeErrorElement("エラーが発生しました。"));
 				target.prop("disabled", false);
 			});
 			return false;
@@ -30,51 +30,58 @@
 			const res = JSON.parse(data);
 			if (res.status === 200) {
 				if (!res.results) {
-					resultsArea.append('<span class="error">該当住所なし。</span>');
+					resultsArea.append(makeErrorElement("該当住所なし。"));
 					return;
 				}
 				resultsArea.append(`<div>検索件数：${res.results.length}</div>`);
 				res.results.forEach((result) => {
-					const table = `
-						<table class="result">
-							<tr>
-								<th>zipcode</th>
-								<td>${result.zipcode}</td>
-							</tr>
-							<tr>
-								<th>prefcode</th>
-								<td>${result.prefcode}</td>
-							</tr>
-							<tr>
-								<th>address1</th>
-								<td>${result.address1}</td>
-							</tr>
-							<tr>
-								<th>address2</th>
-								<td>${result.address2}</td>
-							</tr>
-							<tr>
-								<th>address3</th>
-								<td>${result.address3}</td>
-							</tr>
-							<tr>
-								<th>kana1</th>
-								<td>${result.kana1}</td>
-							</tr>
-							<tr>
-								<th>kana2</th>
-								<td>${result.kana2}</td>
-							</tr>
-							<tr>
-								<th>kana3</th>
-								<td>${result.kana3}</td>
-							</tr>
-						</table>`;
-					resultsArea.append(table);
+					resultsArea.append(makeResultElement(result));
 				});
 			} else {
-				resultsArea.append(`<span class="error">${res.message}</span>`);
+				resultsArea.append(makeErrorElement(res.message));
 			}
+		}
+
+		const makeResultElement = (result) => {
+			return `
+				<table class="result">
+					<tr>
+						<th>zipcode</th>
+						<td>${result.zipcode}</td>
+					</tr>
+					<tr>
+						<th>prefcode</th>
+						<td>${result.prefcode}</td>
+					</tr>
+					<tr>
+						<th>address1</th>
+						<td>${result.address1}</td>
+					</tr>
+					<tr>
+						<th>address2</th>
+						<td>${result.address2}</td>
+					</tr>
+					<tr>
+						<th>address3</th>
+						<td>${result.address3}</td>
+					</tr>
+					<tr>
+						<th>kana1</th>
+						<td>${result.kana1}</td>
+					</tr>
+					<tr>
+						<th>kana2</th>
+						<td>${result.kana2}</td>
+					</tr>
+					<tr>
+						<th>kana3</th>
+						<td>${result.kana3}</td>
+					</tr>
+				</table>`;
+		}
+
+		const makeErrorElement = (message) => {
+			return `<span class="error">${message}</span>`;
 		}
 	});
 })(jQuery);
