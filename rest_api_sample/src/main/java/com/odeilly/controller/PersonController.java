9d7de4f690e4581600b3a2e2b4f3d1d6f2e7e425
odeilly <odeilly@gmail.com>
2024-09-19 14:07:26 +0900

    変更

--- rest_api_sample/src/main/java/com/odeilly/controller/PersonController.java
+++ rest_api_sample/src/main/java/com/odeilly/controller/PersonController.java
@@ -1,12 +1,14 @@
 package com.odeilly.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.odeilly.domain.Person;
 
 @RestController
-public class SampleAController {
+@RequestMapping("/persons")
+public class PersonController {
 
 	@GetMapping(path = "/getSamplePerson")
 	public Person getSamplePerson() {
