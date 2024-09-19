1f534cc65effa5f9428032d9c18124fd19c2f12f
odeilly <odeilly@gmail.com>
2024-09-19 15:00:51 +0900

    Controller修正

--- rest_api_sample/src/main/java/com/odeilly/controller/PersonController.java
+++ rest_api_sample/src/main/java/com/odeilly/controller/PersonController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.odeilly.domain.Person;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person")
 public class PersonController {
 
 	@GetMapping(path = "/getSamplePerson")
 	public Person getSamplePerson() {
 		var person = new Person();
+		person.setId(10);
 		person.setAge(50);;
 		person.setName("Odeilly");
 		return person;
