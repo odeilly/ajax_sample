1ec79676a0499a47fb66c7d9456b4349ce4c620f
odeilly <odeilly@gmail.com>
2024-09-22 23:33:05 +0900

    微修正

--- rest_api_sample/src/main/resources/static/js/person/person.js
+++ rest_api_sample/src/main/resources/static/js/person/person.js
@@ -1,33 +1,29 @@
 (($) => {
-	
-	$(window).on("load", () => {
-		
-		$("[name=button-get-sample-person]").on("click", (event) => {
-			const $target = $(event.currentTarget).prop("disabled", true);
-			const $resultArea = $("#result-get-sample-person").empty().hide();
-			$.ajax({
-				url: $("#url-get-sample-person").attr("action")
-			})
-			.done((response) => {
-				console.log(response);
-				const person = JSON.parse(JSON.stringify(response));
-				const resultHtml = `
-					<dl>
-						<dt>ID</dt>
-						<dd>${person.id}</dd>
-						<dt>Name</dt>
-						<dd>${person.name}</dd>
-						<dt>Age</dt>
-						<dd>${person.age}</dd>
-					</dl>
-				`;
-				$resultArea.append(resultHtml);
-			})
-			.always(() => {
-				$resultArea.show();
-				$target.prop("disabled", false)
-			});
-			return false;
+	$("[name=button-get-sample-person]").on("click", (event) => {
+		const $target = $(event.currentTarget).prop("disabled", true);
+		const $resultArea = $("#result-get-sample-person").empty().hide();
+		$.ajax({
+			url: $("#url-get-sample-person").attr("action")
+		})
+		.done((response) => {
+			console.log(response);
+			const person = JSON.parse(JSON.stringify(response));
+			const resultHtml = `
+				<dl>
+					<dt>ID</dt>
+					<dd>${person.id}</dd>
+					<dt>Name</dt>
+					<dd>${person.name}</dd>
+					<dt>Age</dt>
+					<dd>${person.age}</dd>
+				</dl>
+			`;
+			$resultArea.append(resultHtml);
+		})
+		.always(() => {
+			$resultArea.show();
+			$target.prop("disabled", false)
 		});
+		return false;
 	});
 })(jQuery)
